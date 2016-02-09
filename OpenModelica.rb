61f41b6c80bc188e2564a9cf5086f1ade4f9ffe6
Grant Stephens <grant@stephens.co.za>
2016-02-09 14:26:36 +0200

    Updated to work on head and build libraries

--- OpenModelica.rb
+++ OpenModelica.rb
@@ -1,76 +1,72 @@
 class Openmodelica < Formula
-  desc "OpenModelica is an open-source Modelica-based modeling and simulation environment intended for industrial and academic usage."
+  desc "Open-source modeling and simulation tool"
   homepage "https://openmodelica.org/"
-  url "https://github.com/RexFuzzle/OpenModelica/releases/download/v1.9.3/OpenModelica_v1.9.3_Complete.tar.gz"
-  version "v1.9.3"
-  sha256 "1cd3e5311a507b6dba676d5e91ed210147788d886bcd1405a97598516eaa38cd"
-
-  head "https://github.com/OpenModelica/OpenModelica.git"
-                                         # ,:revision => "4ab25f58d4b2a779b1db9e7feec9cbdbf418cf0c"
-                                         #,:revision => "c3dd385ae1d2e287aa3acce84a97917e427e32ad"
-                                         # or :branch => "develop"
-                                         # or :tag => "1_0_release",
-                                         #    :branch => "v1.9.3"
-  devel do
-    url "https://github.com/RexFuzzle/OpenModelica/releases/download/v1.9.4-dev-OSX/OpenModelica_v1.9.4_Devel.tar.gz"
-    sha256 "7174cd72776ef691129c0dacebc508b317ff51a6c22ce4d64283305106857240"
+  url "https://github.com/OpenModelica/OpenModelica/releases/download/v1.9.3/openmodelica_1.9.3.tar.xz"
+  sha256 "76d53076ae2681183be5f90574e4bd83203d455d1ddc31b93812c392859a667a"
+  head do
+    url "https://github.com/OpenModelica/OpenModelica.git"
+    option "with-library", "Build with OMLibraries"
   end
 
-bottle do
-  root_url "https://github.com/RexFuzzle/OpenModelica/releases/download/v1.9.3"
-  sha256 "1637ed13b11ebb25c6db2a8aac097667f798ae06cd2a54148cf5b88e7314736f" => :el_capitan
-end
-
-	depends_on "qt4"
-  depends_on "autoconf"
-  depends_on "automake"
+  depends_on "qt"
+  depends_on "autoconf" =>:build
+  depends_on "automake" =>:build
   depends_on "boost"
-  depends_on "cmake"
+  depends_on "cmake" =>:build
   depends_on "expat"
   depends_on "gettext"
-  depends_on "git"
   depends_on "gnu-sed"
   depends_on "hdf5"
-  depends_on "hwloc"
-  depends_on "lapack"
+  depends_on "homebrew/dupes/lapack"
   depends_on "liblas"
-  depends_on "libtool"
+  depends_on "libtool" =>:build
   depends_on "lp_solve"
   depends_on "ncurses"
-  depends_on "omniorb"
   depends_on "openblas"
   depends_on "readline"
-  depends_on "sundials"
+  depends_on "sundials" => "without-mpi"
   depends_on "xz"
-	conflicts_with "open-mpi", :because => "Causes issues in compiling. Can be reinstalled afterwards again"
+  conflicts_with "open-mpi", :because => "Causes issues in compiling."
+  conflicts_with "hwloc", :because => "Causes issues in compiling."
+
+  depends_on "omniorb" => :optional
 
   def install
-    ENV['LDFLAGS']='-L/usr/local/opt/openblas/lib'
-    ENV['CPPFLAGS']='-I/usr/local/opt/openblas/include'
-    system "svn ls https://openmodelica.org/svn/OpenModelica --non-interactive --trust-server-cert"
-    system "autoconf"
-    system "./configure", "--disable-debug",
-                          "--with-lapack=-lopenblas",
-                          "--disable-modelica3d",
-                          "--with-omniORB",
-                          "--prefix=#{prefix}"
+    ENV["LDFLAGS"] = "-L#{Formula["openblas"].opt_lib}"
+    ENV["CPPFLAGS"] = "-I#{Formula["openblas"].opt_include}"
+    args = %W[--disable-debug
+              --with-lapack=-lopenblas
+              --disable-modelica3d
+              --prefix=#{prefix}
+              --without-omlibrary]
 
-    system "make -j7 omc"
-    system "make -j7 omlibrary-all"
-    # system "(cd testsuite/partest && ./runtests.pl)"
+    args << "--with-omniORB" if build.with? "omniorb"
+
+    system "autoconf"
+    system "./configure", *args
+    system "make", "omc"
+    if build.with? "library"
+      system "make", "omlibrary-all"
+    end
     prefix.install Dir["build/*"]
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test OpenModelica`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "#{bin}/omc --version"
+    system "#{bin}/omc", "--version"
+    (testpath/"test.mo").write <<-EOS.undent
+    model test
+    Real x;
+    initial equation
+    x = 10;
+    equation
+    der(x) = -x;
+    end test;
+    EOS
+    (testpath/"test.mos").write <<-EOS.undent
+    loadFile("test.mo");
+    simulate(test);
+    EOS
+    system "#{bin}/omc", "test.mos"
+    assert File.exist?("test_res.mat")
   end
 end
